6c26ee3144e0a3acaef2368f9d16b99fd20efb00
Peliah <pelepoupa@gmail.com>
2024-07-08 02:22:23 +0100

    ohayoo

--- src/data/CarouseSlides.js
+++ src/data/CarouseSlides.js
@@ -1,6 +1,8 @@
+import image1 from '../assets/images/image-2.png';
+import image2 from "../assets/images/image 5.png";
+import image3 from "../assets/images/image 8.png";
+import image4 from "../assets/images/image 10.png";
+
 export const slides = [
-    '/src/assets/images/image-2.png',
-    '/src/assets/images/image 5.png',
-    '/src/assets/images/image 8.png',
-    '/src/assets/images/image 10.png',
+    image1, image2, image3, image4
 ];
