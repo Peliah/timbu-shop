9098e77fe8e0ca3fa0097e81eda14102da107666
Peliah <pelepoupa@gmail.com>
2024-07-08 01:09:57 +0100

    dsfbsdf

--- src/data/furnitures.js
+++ src/data/furnitures.js
@@ -2,112 +2,112 @@ export const furniture = [
     {
         "id": 1,
         "name": "Living Room Chair",
-        "image": "/src/assets/images/image-2.png",
+        "image": "../assets/images/image-2.png",
         "description": "Description of chair 1",
         "categories": ["living room", "modern"]
     },
     {
         "id": 2,
         "name": "Ergonomic Office Chair",
-        "image": "/src/assets/images/image 5.png",
+        "image": "../assets/images/image 5.png",
         "description": "Description of chair 2",
         "categories": ["office", "ergonomic"]
     },
     {
         "id": 3,
         "name": "Kitchen Chair",
-        "image": "/src/assets/images/image 8.png",
+        "image": "../assets/images/image 8.png",
         "description": "Description of chair 3",
         "categories": ["kitchen"]
     },
     {
         "id": 4,
         "name": "Lounge Chair",
-        "image": "/src/assets/images/image 10.png",
+        "image": "../assets/images/image 10.png",
         "description": "Description of chair 4",
         "categories": ["lounge"]
     },
     {
         "id": 5,
         "name": "Modern Chair",
-        "image": "/src/assets/images/image 12.png",
+        "image": "../assets/images/image 12.png",
         "description": "Description of chair 5",
         "categories": ["modern"]
     },
     {
         "id": 6,
         "name": "Bedroom Chair",
-        "image": "/src/assets/images/image 14.png",
+        "image": "../assets/images/image 14.png",
         "description": "Description of chair 6",
         "categories": ["bedroom"]
     },
     {
         "id": 7,
         "name": "Office Chair",
-        "image": "/src/assets/images/image 15.png",
+        "image": "../assets/images/image 15.png",
         "description": "Description of chair 7",
         "categories": ["office"]
     },
     {
         "id": 8,
         "name": "Kitchen Stool",
-        "image": "/src/assets/images/image 16.png",
+        "image": "../assets/images/image 16.png",
         "description": "Description of chair 8",
         "categories": ["kitchen"]
     },
     {
         "id": 9,
         "name": "Metal Kitchen Chair",
-        "image": "/src/assets/images/image 17.png",
+        "image": "../assets/images/image 17.png",
         "description": "Description of chair 9",
         "categories": ["kitchen"]
     },
     {
         "id": 10,
         "name": "Comfort Lounge Chair",
-        "image": "/src/assets/images/image 18.png",
+        "image": "../assets/images/image 18.png",
         "description": "Description of chair 10",
         "categories": ["lounge"]
     },
     {
         "id": 11,
         "name": "Vintage Bedroom Chair",
-        "image": "/src/assets/images/image 19.png",
+        "image": "../assets/images/image 19.png",
         "description": "Description of chair 11",
         "categories": ["bedroom"]
     },
     {
         "id": 12,
         "name": "Living Room Recliner",
-        "image": "/src/assets/images/image 20.png",
+        "image": "../assets/images/image 20.png",
         "description": "Description of chair 12",
         "categories": ["living room"]
     },
     {
         "id": 13,
         "name": "Fabric Lounge Chair",
-        "image": "/src/assets/images/image 21.png",
+        "image": "../assets/images/image 21.png",
         "description": "Description of chair 13",
         "categories": ["lounge"]
     },
     {
         "id": 14,
         "name": "Office Armchair",
-        "image": "/src/assets/images/image 22.png",
+        "image": "../assets/images/image 22.png",
         "description": "Description of chair 14",
         "categories": ["office"]
     },
     {
         "id": 15,
         "name": "Living Room Sofa",
-        "image": "/src/assets/images/image 23.png",
+        "image": "../assets/images/image 23.png",
         "description": "Description of chair 15",
         "categories": ["living room"]
     },
     {
         "id": 16,
         "name": "Modern Lounge Chair",
-        "image": "/src/assets/images/image 24.png",
+        "image": "../assets/images/image 24.png",
         "description": "Description of chair 16",
         "categories": ["modern"]
     }
