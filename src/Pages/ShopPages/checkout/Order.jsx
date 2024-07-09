105387e189d97bcd083d2c30d6bd435c4dfaa0fd
Pelayah Epoupa <pelepoupa@gmail.com>
2024-07-09 14:13:27 +0100

    dasf

--- src/Pages/ShopPages/checkout/Order.jsx
+++ src/Pages/ShopPages/checkout/Order.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useCart } from '../../../hooks/CartContext/CartProvider'
 
 const Order = () => {
+    const { cart } = useCart();
     return (
         <div>
-            
+            <h2 className='font-semibold text-2xl pt-6'>
+                My Shopping Cart
+            </h2>
+            <div>
+                {cart.map((item) => (
+                    <div className='w-full '>
+                        <div>
+                            <img src={item.image} alt={item.name} className="h-32 w-32 object-cover rounded-md" />
+                        </div>
+                        <div></div>
+                        <h4>${item.price},000</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
--- src/component/HeroeSection/Heroe.jsx
+++ src/component/HeroeSection/Heroe.jsx
@@ -5,17 +5,17 @@ import arrow from "../../assets/images/right-arrow-white.png"
 
 const Heroe = () => {
     return (
-        <div className='w-full min-h-screen bg-secondary bg-gradient-to-r from-primary from-60% to-secondary to-40% lg:flex hidden flex-row items-center justify-between md:px-36 px-10 '>
-            <div className='flex flex-col w-1/4 gap-10 text-secondary justify-end '>
-                <h1 className='font-Montserrat text-6xl font-bold'>
+        <div className='w-full min-h-screen bg-secondary bg-gradient-to-r from-primary from-60% to-secondary to-40% md:flex hidden flex-row items-center justify-between lg:px-36 sm:px-10 px-4 '>
+            <div className='flex flex-col lg:w-1/4 w-3/6 gap-10 text-secondary justify-end '>
+                <h1 className='font-Montserrat lg:text-6xl text-5xl font-bold'>
                     Furniture that speaks your personality
                 </h1>
                 <p className='font-Montserrat font-normal text-2xl'>
                     Discover the embodiment of your unique style today
                 </p>
-                <button className='bg-secondary w-fit flex px-6 py-3 font-Montserrat font-semibold text-xl text-primary rounded-xl gap-4 '>
+                <button className='bg-secondary text-nowrap  w-fit flex px-2 py-3 items-center font-Montserrat font-semibold lg:text-xl text-lg text-primary rounded-xl gap-4 '>
                     Explore Now
-                    <div className='bg-primary rounded-lg px-3'><img src={arrow} /></div>
+                    <div className='bg-primary rounded-lg px-2  w-full'><img cl src={arrow} /></div>
                 </button>
             </div>
 
