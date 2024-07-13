d447ab732504a587d655e67e1ae777b1a2d12e8c
Pelayah Epoupa <pelepoupa@gmail.com>
2024-07-13 13:12:58 +0100

    sdasad

--- src/Pages/ShopPages/Cart.jsx
+++ src/Pages/ShopPages/Cart.jsx
@@ -70,9 +70,9 @@ const Cart = () => {
                     {/* <button className='bg-primary rounded-l-md  px-1 hover:scale-110' onClick={() => updateQuantity(item.id, item.quantity + 1)}><PlusIcon color='#fff' /></button>
                     <h4 className=' px-1'>{item.quantity}</h4>
                     <button className=' bg-secondary rounded-r-md  px-1 hover:scale-110' onClick={() => updateQuantity(item.id, item.quantity - 1)}> <MinusIcon color='#254D4D' /></button> */}
-                    <button className='bg-primary rounded-l-md  p-2 hover:scale-110' onClick={() => updateQuantity(item.id, item.quantity + 1)}><PlusIcon color='#fff' /></button>
+                    <button className='bg-primary rounded-l-md  px-2 hover:scale-110' onClick={() => updateQuantity(item.id, item.quantity + 1)}><PlusIcon color='#fff' /></button>
                     <h4 className='px-3 p-2'>{item.quantity}</h4>
-                    <button className=' bg-secondary rounded-r-md  p-2 hover:scale-110' onClick={() => updateQuantity(item.id, item.quantity - 1)}> <MinusIcon color='#254D4D' /></button>
+                    <button className=' bg-secondary rounded-r-md  px-2 hover:scale-110' onClick={() => updateQuantity(item.id, item.quantity - 1)}> <MinusIcon color='#254D4D' /></button>
 
                   </div>
                 </div>
