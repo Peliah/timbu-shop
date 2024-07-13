542c78bc3494c259ab4db876312371a77153fc54
Peliah <pelepoupa@gmail.com>
2024-07-13 05:12:35 +0100

    sdf fsdf

--- src/api/Client.jsx
+++ src/api/Client.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 export default axios.create({
-    // baseURL: 'api',
-    baseURL: import.meta.env.VITE_BASE_URL,
+    baseURL: '/api',
+    // baseURL: import.meta.env.VITE_BASE_URL,
     params: {
         organization_id: import.meta.env.VITE_ORGANISATION_ID,
         Appid: import.meta.env.VITE_APP_ID,
