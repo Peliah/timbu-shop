f7110246bd4f955bd8a67b9f3fa29712b1b9052b
Peliah <pelepoupa@gmail.com>
2024-07-13 14:06:46 +0100

    deployed

--- src/api/Client.jsx
+++ src/api/Client.jsx
@@ -2,12 +2,12 @@ import axios from "axios";
 
 
 // const baseURL = import.meta.env.MODE === 'development' ? '/api' : 'https://api.timbu.cloud';
-const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+// const proxyURL = 'https://cors-anywhere.herokuapp.com/';
 
 export default axios.create({
     // baseURL: '/api',
-    // baseURL: import.meta.env.VITE_BASE_URL,
-    baseURL: proxyURL + import.meta.env.VITE_BASE_URL,
+    baseURL: import.meta.env.VITE_BASE_URL,
+    // baseURL: proxyURL + import.meta.env.VITE_BASE_URL,
     params: {
         organization_id: import.meta.env.VITE_ORGANISATION_ID,
         Appid: import.meta.env.VITE_APP_ID,
