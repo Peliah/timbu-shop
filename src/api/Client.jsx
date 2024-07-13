d01c002e0ad7f6940e05f8b912243ffd85f130ab
Pelayah Epoupa <pelepoupa@gmail.com>
2024-07-13 12:05:13 +0100

    sadfdsf

--- src/api/Client.jsx
+++ src/api/Client.jsx
@@ -6,20 +6,11 @@ const proxyURL = 'https://cors-anywhere.herokuapp.com/';
 
 export default axios.create({
     // baseURL: '/api',
-    // baseURL: baseURL,
+    // baseURL: import.meta.env.VITE_BASE_URL,
     baseURL: proxyURL + import.meta.env.VITE_BASE_URL,
     params: {
         organization_id: import.meta.env.VITE_ORGANISATION_ID,
         Appid: import.meta.env.VITE_APP_ID,
         Apikey: import.meta.env.VITE_API_KEY,
     },
-    // withCredentials: false,
-    // headers: {
-    //     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-    // }
-    // headers: {
-    //     'Access-Control-Allow-Origin': '*', // Adjust as needed
-    //     'Access-Control-Allow-Methods': 'GET, POST, PUT, OPTIONS',
-    //     'Access-Control-Allow-Headers': 'Content-Type',
-    // }
 })
